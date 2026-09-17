9952be9ca20315eb06b42ff4a11abb023b76868c
jay Dobariya <jaydobariya1067@gmail.com>
2026-09-17 16:19:02 +0530

    Get user api

--- SpringDataJpaDemo/src/main/java/com/example/SpringDataJpaDemo/Controller/UserController.java
+++ SpringDataJpaDemo/src/main/java/com/example/SpringDataJpaDemo/Controller/UserController.java
@@ -7,10 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -22,5 +19,10 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody CreateUserDto createUserDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveUser(createUserDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<UserDto> getUser(@RequestBody CreateUserDto getUserDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getuser(getUserDto));
     }
 }
