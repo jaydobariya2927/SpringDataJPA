14e3ffc68c442d5853df06bdd7731bf954d59ccb
jay Dobariya <jaydobariya1067@gmail.com>
2026-09-18 15:02:29 +0530

    made APIs

--- SpringDataJpaDemo/src/main/java/com/example/SpringDataJpaDemo/Controller/UserController.java
+++ SpringDataJpaDemo/src/main/java/com/example/SpringDataJpaDemo/Controller/UserController.java
@@ -5,9 +5,12 @@ import com.example.SpringDataJpaDemo.dto.CreateUserDto;
 import com.example.SpringDataJpaDemo.dto.UserDto;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -22,7 +25,28 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<UserDto> getUser(@RequestBody CreateUserDto getUserDto) {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.getuser(getUserDto));
+    public ResponseEntity<List<UserDto>> getUsers() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getusers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserByID(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUserById(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<UserDto> updateUser(@RequestBody CreateUserDto updateUserDto, @PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(id, updateUserDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserDto> patchUser(@RequestBody CreateUserDto patchUserDto,@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.patchUSer(id, patchUserDto));
     }
 }
--- SpringDataJpaDemo/src/main/java/com/example/SpringDataJpaDemo/Service/UserService.java
+++ SpringDataJpaDemo/src/main/java/com/example/SpringDataJpaDemo/Service/UserService.java
@@ -6,7 +6,13 @@ import com.example.SpringDataJpaDemo.dto.CreateUserDto;
 import com.example.SpringDataJpaDemo.dto.UserDto;
 import com.example.SpringDataJpaDemo.entities.User;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @Service
@@ -21,5 +27,53 @@ public class UserService {
         User savedUser = userRepository.save(user);
 
         return new UserDto(savedUser.getId(), savedUser.getName(), savedUser.getEmail());
+    }
+
+
+    public List<UserDto> getusers() {
+        List<User> users = userRepository.findAll();
+        List<UserDto> userDtoList = new ArrayList<>();
+        for (User user : users) {
+            UserDto userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+            userDtoList.add(userDto);
+        }
+        return userDtoList;
+    }
+
+
+    public UserDto getUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow();
+        return new UserDto(user.getId(), user.getName(), user.getEmail());
+
+    }
+
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+  @Transactional
+    public UserDto updateUser(Long id, CreateUserDto updateUserDto) {
+        User user = userRepository.findById(id).orElseThrow();
+        user.setName(updateUserDto.getName());
+        user.setEmail(updateUserDto.getEmail());
+
+        return new UserDto(user.getId(), user.getName(), user.getEmail());
+    }
+
+
+
+    @Transactional
+    public UserDto patchUSer(Long id, CreateUserDto patchUserDto) {
+        User user = userRepository.findById(id).orElseThrow();
+
+        if (patchUserDto.getName() != null) {
+            user.setName(patchUserDto.getName());
+        }
+        if (patchUserDto.getEmail() != null) {
+            user.setEmail(patchUserDto.getEmail());
+        }
+
+        return new UserDto(user.getId(), user.getName(), user.getEmail());
     }
 }
